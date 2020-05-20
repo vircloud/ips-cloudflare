62e8094732b90ae5632b172f3bc43172791ebc48
vircloud.net <32838469+vircloud@users.noreply.github.com>
2020-05-20 20:45:34 +0800

    20200520_V3
    API 有限制

--- add-blacklist.sh
+++ add-blacklist.sh
@@ -9,14 +9,27 @@ ZONESID=""
 ACCOUNTID=""
 # 填 IP 黑名单存放位置
 IPS="/root/blacklist.txt"
+# API 调用每 5 分钟限制 1200
+LIMIT="1200"
+# 休息 5 分钟
+SLEEP="300"
 # 循环提交 IPs 到 Cloudflare  防火墙黑名单
+I="1"
 for IPADDR in `cat $IPS`
 do
- echo "正在添加 IP：$IPADDR"
+ if [ "$I" -lt "$LIMIT" ]
+ then
+   echo "正在添加第 $I 个 IP： $IPADDR"
 #这是域名级屏蔽 curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$ZONESID/firewall/access_rules/rules" \
 #这是账户级屏蔽 curl -s -X POST "https://api.cloudflare.com/client/v4/accounts/$ACCOUNTID/firewall/access_rules/rules" \
    -H "X-Auth-Email: $CFEMAIL" \
    -H "X-Auth-Key: $CFAPIKEY" \
    -H "Content-Type: application/json" \
    --data '{"mode":"challenge","configuration":{"target":"ip_range","value":"'$IPADDR'"},"notes":""}'
+   I=`expr $I + 1`
+ else
+  echo "到达 API 调用限制，休息 5 分钟......"
+  sleep $SLEEP
+  I="1"
+ fi
 done
